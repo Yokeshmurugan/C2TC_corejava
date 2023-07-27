2dfc0cb5a42e933e7cf40b341af957491a890a95
Yokesh <yokesh111suji@gmail.com>
2023-07-27 14:29:56 +0530

    FIRST COMMIT

---
+++ Fullstack/src/fullstack/Basics.java
@@ -0,0 +1,22 @@
+package fullstack;
+
+import java.util.Scanner;
+
+public class Basics {
+	public static void main(String[] args) {
+		Scanner Sc = new Scanner(System.in);
+		System.out.println("Enter The A Value");
+		int a = Sc.nextInt();
+		System.out.println("Enter The A Value");
+		int b = Sc.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a|b);
+		System.out.println(a%b);
+}
+
+}
+	
+	
